af38bc25be15d91236510d3a263f024f4ad67fef
2001houpe <71071656+2001houpe@users.noreply.github.com>
2020-12-30 13:07:38 +0100

    Úkol
    Úkol jsem splnil samostatně, bez pomoci. Přidal jsem vkládání čísel pro změnu barvy pro zelenou a modrou. Proto chci jedničku.

--- main.py
+++ main.py
@@ -13,17 +13,27 @@ class Application(tk.Tk):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        self. varR = StringVar()
+        self.varR = StringVar()
         self.varR.set(0)
         self.varR.trace("w", self.entryUpdate)
         self.entryR = Entry(textvariable = self.varR)
         self.entryR.pack()
         self.scaleR = tk.Scale (from_=0, to=255, orient=HORIZONTAL,background = "#ff0000",command = self.change ,length=self.length)
         self.scaleR.pack()
-
+        
+        self.varG = StringVar()
+        self.varG.set(0)
+        self.varG.trace("w", self.entryUpdate)
+        self.entryG = Entry(textvariable = self.varG)
+        self.entryG.pack()
         self.scaleG = tk.Scale (from_=0, to=255, orient=HORIZONTAL, background = "#00ff00",command = self.change, length=self.length)
         self.scaleG.pack()
-
+        
+        self.varB = StringVar()
+        self.varB.set(0)
+        self.varB.trace("w", self.entryUpdate)
+        self.entryB = Entry(textvariable = self.varB)
+        self.entryB.pack()
         self.scaleB = tk.Scale (from_=0, to=255, orient=HORIZONTAL, background = "#0000ff", command = self.change,length=self.length)
         self.scaleB.pack()
 
@@ -44,15 +54,33 @@ class Application(tk.Tk):
             self.varR.set(0)
             r =0
         self.scaleR.set(r)
-    
         
+        g = self.varG.get()
+        if g.isdigit():
+            g = int(g)
+        else:
+            self.varG.set(0)
+            g =0
+        self.scaleG.set(g)
+        
+        b = self.varB.get()
+        if b.isdigit():
+            b = int(b)
+        else:
+            self.varB.set(0)
+            b =0
+        self.scaleB.set(b)
+        
+
     
-    
+      
     def change(self , event):
         r = self.scaleR.get()
-        self.varR.set(r)
+        
         g = self.scaleG.get()
+        
         b = self.scaleB.get()
+        
         hashcolor = "#%02x%02x%02x" % (r, g, b)
         print(hashcolor) 
         self.canvas.config(background = hashcolor)
